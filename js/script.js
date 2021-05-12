24c3a8d8b703d82fb8f82ddc3cfe51fd2d83c907
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-05-12 20:53:19 +0200

    Add const remove class "active" from all articles

--- js/script.js
+++ js/script.js
@@ -8,8 +8,24 @@ document.getElementById('test-button').addEventListener('click', function(){
 const titleClickHandler = function(){
     console.log('Link was clicked!');
     /* remove class 'active' from all article links */
-    /* add class 'active' to the clicked link */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+    /* add class 'active' to the clicked link 
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.add('clicked');
+    }*/
+
     /* remove class 'active' from all articles*/
+    const activeArticles = document.querySelectorAll('article');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
     /* get 'href' attribute from the clicked link */
     /* find the correct article using the selecetor (value of 'href' attribute) */
     /* add class 'active' to the correct article */
