9eb01b98e3e94946221d516ec8c174e742e02131
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-05-14 00:17:33 +0200

    Add function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
-/*
-document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  });
-*/
-
-const titleClickHandler = function(){
+{const titleClickHandler = function(event){
+    event.preventDefault();  
     console.log('Link was clicked!');
-    event.preventDefault();
+    
     const clickedElement = this;
 
     /* remove class 'active' from all article links */
-    const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeLinks = document.querySelectorAll('.titles a.active'); 
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
@@ -45,3 +39,44 @@ const titleClickHandler = function(){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(customSelector = ''){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  function clearMessages(){
+    document.getElementById('messages').innerHTML = '';
+  }
+
+  /* for each article */
+  const articles = document.querySelector(optArticleSelector);
+
+  let html = '';
+
+  for(let article of articles){
+  }
+  /* get the article id */
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+    
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    const linkHTML = templates.articleLink(linkHTMLData);
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
+}
